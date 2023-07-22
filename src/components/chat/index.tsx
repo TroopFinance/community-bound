5ab17fe2a5395fc7a5665e5a42238f48408aec91
chaffie <c.azzaoui@live.nl>
2023-07-22 10:08:33 +0200

    (chore): add important confetti button

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { List, ListItem, ListItemText, Divider } from '@mui/material'
+import { List, ListItem, ListItemText, Divider, Box, Button } from '@mui/material'
 import axios from 'axios'
 import { ethers } from 'ethers'
 import * as PushAPI from '@pushprotocol/restapi'
 import ChatInput from './chatInput'
 import useSafeAddress from '@/hooks/useSafeAddress'
 import { motion } from 'framer-motion'
+import ConfettiExplosion from 'react-confetti-explosion'
 
 export interface IFeeds {
   msg: IMessageIPFS
@@ -115,28 +116,26 @@ const chatMsgVariants = {
 const ChatFeed: React.FC = () => {
   const [chats, setChats] = useState<IMessageIPFS[]>([]) // Updated to IMessageIPFS[]
   const safeAddie = useSafeAddress()
+  const [isExploding, setIsExploding] = React.useState(false)
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if (window.ethereum) {
-          const provider = new ethers.providers.Web3Provider(window.ethereum)
-          const signer = provider.getSigner()
-          console.log({ safeAddie })
-          // Fetch encrypted data using the provided API
+        console.log({ safeAddie })
+        // Fetch encrypted data using the provided API
 
-          // Fetch chats using the actual API
-          const response = await PushAPI.chat.chats({
-            account: `eip155:${safeAddie}`,
-            toDecrypt: false,
-          })
+        // Fetch chats using the actual API
+        const response = await PushAPI.chat.chats({
+          account: `eip155:${safeAddie}`,
+          toDecrypt: false,
+        })
 
-          const groupResponse = await PushAPI.chat.getGroupByName({
-            groupName: 'last try',
-          })
-          // Process the fetched messages and convert them to the IMessageIPFS type
-          setChats(response as unknown as IMessageIPFS[])
-          console.log({ response, groupResponse })
-        }
+        const groupResponse = await PushAPI.chat.getGroupByName({
+          groupName: 'last try',
+        })
+        // Process the fetched messages and convert them to the IMessageIPFS type
+        setChats(response as unknown as IMessageIPFS[])
+        console.log({ response, groupResponse })
       } catch (error) {
         console.error('Error fetching chats:', error)
       }
@@ -156,7 +155,6 @@ const ChatFeed: React.FC = () => {
           messageContent: message,
           messageType: 'Text',
           receiverAddress: 'b0a1d13e6baecd9ce8635e777fe64b1a2e92a5c8e06ddd44111955b1a5083f02',
-          account: signer._address,
           signer: signer,
         })
 
@@ -188,15 +186,35 @@ const ChatFeed: React.FC = () => {
   return (
     <List sx={{ width: '100%', height: '80vh', backgroundColor: 'black' }}>
       {chats.map((chat) => (
-        <React.Fragment key={chat.timestamp}>
-          {/* Use a unique key for the fragment */}
-          <motion.div variants={chatMsgVariants} initial="initial" animate="animate" exit="exit">
-            <ListItem>
-              <ListItemText primary={chat.messageContent} /> {/* Access the property to render */}
-            </ListItem>
-          </motion.div>
-          <Divider />
-        </React.Fragment>
+        <motion.div
+          variants={chatMsgVariants}
+          style={{
+            width: '50%',
+            padding: '8px',
+            marginBottom: '8px',
+            marginLeft: '8px',
+
+            border: 'solid 0.5px white',
+            borderRadius: '8px',
+          }}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          <Box>
+            <ListItemText primary={chat.messageContent} /> {/* Access the property to render */}
+            <Button variant="contained" color="primary" onClick={() => setIsExploding(true)}>
+              Chapeau!
+              {isExploding ? (
+                <ConfettiExplosion
+                  duration={2000}
+                  style={{ position: 'absolute', bottom: 1, right: 1 }}
+                  onComplete={() => setIsExploding(false)}
+                />
+              ) : null}
+            </Button>
+          </Box>
+        </motion.div>
       ))}
       <ChatInput onSendMessage={handleSendMessage} />
     </List>
